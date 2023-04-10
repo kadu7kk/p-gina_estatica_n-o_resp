12c63a72f9002da90e81b5b6a939619c86f18a07
Carlos Eduardo de Melo <118300893+kadu7kk@users.noreply.github.com>
2023-04-10 20:34:40 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything; 
+}
+
+function circleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
